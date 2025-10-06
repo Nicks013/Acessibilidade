6d0f620c7fb24528791ad6319073875ad6ee3d5b
Nicks013 <166658577+Nicks013@users.noreply.github.com>
2025-10-06 07:36:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function (){
+     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+     opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+     const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+     botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
+   
+    });
+ 
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alternaContraste = document.getElementById('alterna-contraste');
+ 
+    let tamanhoAtualFonte = 1;
+ 
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+ 
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+ 
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste');
+    });
+});
+ 
+// Lembre-se de atualizar o seletor para a seção "Sobre"
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#sobre', { delay: 500 }); // Atualizado para #sobre
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
